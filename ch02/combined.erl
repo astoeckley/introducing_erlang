27623dde3f8b099c4fa06745bfaefff8a1063138
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-06 22:27:35 -0800

    example 2-5: refactor to use 'import'

--- ch02/combined.erl
+++ ch02/combined.erl
@@ -1,6 +1,8 @@
 -module(combined).
 -export([height_to_mph/1]).
+-import(drop, [fall_velocity/1]).
+-import(convert, [mps_to_mph/1]).
 
 height_to_mph(Meters) ->
-  convert:mps_to_mph(drop:fall_velocity(Meters)).
+  mps_to_mph(fall_velocity(Meters)).
 
