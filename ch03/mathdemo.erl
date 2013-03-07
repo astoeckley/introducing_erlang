e3facb275b5017935f8879182e530e3a03f52be0
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-06 23:15:21 -0800

    example 3-4: more concise guards for absolute values

--- ch03/mathdemo.erl
+++ ch03/mathdemo.erl
@@ -2,5 +2,5 @@
 -export([absolute_value/1]).
 
 absolute_value(Number) when Number <  0 -> -Number;
-absolute_value(Number) when Number == 0 -> 0;
-absolute_value(Number) when Number >  0 -> Number.
+absolute_value(0) -> 0;
+absolute_value(Number) -> Number.
