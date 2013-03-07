b4d20b764eb89fb41c484f9ef27367f1b764c387
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-06 22:34:53 -0800

    example 2.7: edoc for drop

--- ch02/drop.erl
+++ ch02/drop.erl
@@ -1,3 +1,11 @@
+%% @author Simon St.Laurant <simonstl@simonstl.com> [http://simonstl.com]
+%% @doc Functions calculating velocities archieved by objects
+%% dropped in a vaccuum.
+%% @reference from <a href="http://shop.oreilly.com/product/0636920025818.do">Introducing Erlang</a>,
+%% O'Reilly Media, Inc., 2012
+%% @copyright 2012 by Simon St.Laurent
+%% @version 0.1
+
 -module(drop).
 -export([fall_velocity/1]).
 
