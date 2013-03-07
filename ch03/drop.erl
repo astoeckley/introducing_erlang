618e64d09dcdccb46080a27bf4b2829a5a643870
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-06 23:29:16 -0800

    example 3-8: refactor to private functions

--- ch03/drop.erl
+++ ch03/drop.erl
@@ -1,5 +1,7 @@
 -module(drop).
--export([fall_velocity/2]).
+-export([fall_velocity/1]).
+
+fall_velocity({Planemo, Distance}) -> fall_velocity(Planemo, Distance).
 
 fall_velocity(earth, Distance) when Distance >= 0 -> math:sqrt(2 * 9.8 * Distance);
 fall_velocity(moon, Distance) when Distance >= 0 -> math:sqrt(2 * 1.6 * Distance);
