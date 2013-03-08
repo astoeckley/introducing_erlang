20b6d8e854018aa30447505771789140edf19d25
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-07 21:37:16 -0800

    example 4-8: limit recursion

--- ch04/count.erl
+++ ch04/count.erl
@@ -1,5 +1,15 @@
 -module(count).
--export([countdown/1]).
+-export([countdown/1, countup/1]).
+
+countup(Limit) ->
+  countup(1, Limit).
+
+countup(Count, Limit) when Count =< Limit ->
+  io:format("~w!~n", [Count]),
+  countup(Count+1, Limit);
+
+countup(Count, Limit) ->
+  io:format("Finished.~n").
 
 countdown(From) when From > 0 ->
   io:format("~w!~n", [From]),
