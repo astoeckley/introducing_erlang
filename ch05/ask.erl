d3d11398c219c404e89d8f72c08f445f7f833164
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-09 13:32:58 -0800

    example 5-4: get character from terminal input

--- ch05/ask.erl
+++ ch05/ask.erl
@@ -1,5 +1,12 @@
 -module(ask).
--export([term/0]).
+-export([chars/0, term/0]).
+
+chars() ->
+  io:format("Which planemo are you on?~n"),
+  io:format(" 1. Earth~n"),
+  io:format(" 2. Earth's Moon~n"),
+  io:format(" 3. Mars~n"),
+  io:get_chars("Which? > ", 1).
 
 term() ->
   Input = io:read("What {planemo, distance} ? >>"),
