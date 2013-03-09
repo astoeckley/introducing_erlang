22af2d8a7c4cfb46561c6ee249973780ee998e00
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-09 13:26:27 -0800

    example 5-3: handling bad results

--- ch05/ask.erl
+++ ch05/ask.erl
@@ -5,8 +5,18 @@ term() ->
   Input = io:read("What {planemo, distance} ? >>"),
   process_term(Input).
 
-process_term({ok, Term}) when is_tuple(Term) -> drop:fall_velocity(Term);
+process_term({ok, Term}) when is_tuple(Term) ->
+  Velocity = drop:fall_velocity(Term),
+  io:format("Yields ~w. ~n", [Velocity]),
+  term();
 
-process_term({ok, _}) -> io:format("You must enter a tuple.~n");
+process_term({ok, quit}) ->
+  io:format("Goodbye.~n");
 
-process_term({error, _}) -> io:format("You must enter a tuple with correct syntax.~n").
+process_term({ok, _}) -> 
+  io:format("You must enter a tuple.~n"),
+  term();
+
+process_term({error, _}) ->
+  io:format("You must enter a tuple with correct syntax.~n"),
+  term().
