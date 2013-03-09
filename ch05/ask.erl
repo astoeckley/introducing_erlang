cc7f3a031817f219b4d4e7d0e86f8c318c8c817d
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-09 13:15:21 -0800

    example 5-2: handle bad terminal input

--- ch05/ask.erl
+++ ch05/ask.erl
@@ -3,5 +3,10 @@
 
 term() ->
   Input = io:read("What {planemo, distance} ? >>"),
-  Term = element(2, Input),
-  drop:fall_velocity(Term).
+  process_term(Input).
+
+process_term({ok, Term}) when is_tuple(Term) -> drop:fall_velocity(Term);
+
+process_term({ok, _}) -> io:format("You must enter a tuple.~n");
+
+process_term({error, _}) -> io:format("You must enter a tuple with correct syntax.~n").
