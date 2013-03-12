58f378af92e924f0bc9da9478892d704724ccaef
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-12 14:02:01 -0700

    example 6-5: pascal's triangle

--- ch06/pascal.erl
+++ ch06/pascal.erl
@@ -1,5 +1,14 @@
 -module (pascal).
--export ([add_row/1]).
+-export ([triangle/1]).
+
+triangle(Rows) -> triangle([[0,1,0]], 1, Rows).
+
+triangle(List, Count, Rows) when Count >= Rows ->
+	lists:reverse(List);
+	
+triangle(List, Count, Rows) ->
+	[Previous | _] = List,
+	triangle([add_row(Previous) | List], Count+1, Rows).
 
 add_row(Initial) -> add_row(Initial, 0, []).
 
