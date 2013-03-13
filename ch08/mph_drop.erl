7737bef93a5478b2195ec1c4c4a823fc2e18f4cc
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-12 23:04:24 -0700

    example 8-9: trapping & reporting error

--- ch08/mph_drop.erl
+++ ch08/mph_drop.erl
@@ -2,6 +2,7 @@
 -export ([mph_drop/0]).
 
 mph_drop() ->
+	process_flag(trap_exit, true),
 	Drop=spawn_link(drop, drop, []),
 	convert(Drop).
 	
@@ -10,6 +11,8 @@ convert(Drop) ->
 		{Planemo, Distance} ->
 			Drop ! { self(), Planemo, Distance },
 			convert(Drop);
+		{'EXIT', Pid, Reason} ->
+			io:format("FAILURE: ~p died because of ~p.~n", [Pid, Reason]);
 		{Planemo, Distance, Velocity} ->
 			MphVelocity = 2.23693629 * Velocity,
 			io:format("On ~p, a fall of ~p meters yields a velocity of ~p mph.~n", [Planemo, Distance, MphVelocity]),
