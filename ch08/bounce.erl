630e51b556642bb2ccdaa74b88588788996e4d9f
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-12 22:26:49 -0700

    example 8-4: refactor of message counter

--- ch08/bounce.erl
+++ ch08/bounce.erl
@@ -2,7 +2,8 @@
 -export ([report/1]).
 
 report(Count) ->
-	receive
+	NewCount = receive
 		X -> io:format("Received #~p: ~p~n", [Count,X]),
-		report(Count+1)
-	end.
+		Count+1
+	end,
+	report(NewCount).
