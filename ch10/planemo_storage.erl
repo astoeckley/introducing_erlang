b357e373326a3d7bff24aa4b1ed41657aaffd79c
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-03-14 22:46:49 -0700

    example 10-6: partial table insert

--- ch10/planemo_storage.erl
+++ ch10/planemo_storage.erl
@@ -4,4 +4,12 @@
 
 setup() ->
 	PlanemoTable = ets:new(planemos, [named_table, {keypos, #planemo.name}]),
+	
+	ets:insert(planemos, 
+		#planemo{ name=mercury, gravity=3.7, diameter=4878, distance_from_sun=57.9}),
+	ets:insert(planemos, 
+		#planemo{ name=venus, gravity=8.9, diameter=12104, distance_from_sun=108.2}),
+	ets:insert(planemos, 
+		#planemo{ name=earth, gravity=9.8, diameter=12756, distance_from_sun=149.6}),
+		
 	ets:info(PlanemoTable).
